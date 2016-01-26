22b0c31848a3f59999a15036bf904cb934bdd095
Joshua Branson <bransoj@hotmail.com>
2016-01-26 18:10:07 -0500

    I removed ";" as endless commend line.   not needed.

--- evil-dvorak.el
+++ evil-dvorak.el
@@ -56,20 +56,6 @@
   evil-dvorak-mode turn-on-evil-dvorak-mode
   "Global mode to let you use evil with dvorak friendly keybindings.")
 
-(defun endless/comment-line (n)
-  "Comment or uncomment current line and leave point after it.
-With positive prefix, apply to N lines including current one.
-With negative prefix, apply to -N lines above."
-  (interactive "p")
-  ;;if we are in web-mode, use comment-or-uncomment
-  (if (eq major-mode 'web-mode)
-      (web-mode-comment-or-uncomment)
-    (comment-or-uncomment-region)
-    (line-beginning-position)
-    (goto-char (line-end-position n))
-    (forward-line 1)
-    (back-to-indentation)))
-
 (evil-define-key 'visual evil-dvorak-mode-map
   "s" 'evil-forward-char
   "n" 'evil-backward-char
