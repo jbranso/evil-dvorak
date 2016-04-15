4a877ef2752b40cc2348ee989ad153fc1fa20561
Joshua Branson <bransoj@hotmail.com>
2016-04-15 19:37:51 -0400

    This is the first commit where I did the initial changes that AlejandroCatalina did for evil-dvorak.

--- evil-dvorak.el
+++ evil-dvorak.el
@@ -69,20 +69,10 @@
   "e" 'evil-forward-word-begin
   "O" 'evil-backward-WORD-end
   "E" 'evil-forward-WORD-end
-  (kbd "<backspace>") 'avy-goto-char
-  )
+  (kbd "<backspace>") 'avy-goto-char)
 
 ;; I don't like the normal keys that vim users use for up and down, so
 ;; if dvorak-funky-h-and-t == 1, then I'll swap h and t.
-(defvar evil-dvorak-funky-h-and-t)
-(defvar evil-dvorak-use-for-web-mode)
-(if (equal evil-dvorak-funky-h-and-t 1)
-    (evil-define-key 'normal evil-dvorak-mode-map
-      "t" 'evil-previous-line
-      "h" 'evil-next-line)
-  (evil-define-key 'normal evil-dvorak-mode-map
-    "t" 'evil-previous-line
-    "h" 'evil-next-line))
 
 (evil-define-key 'normal evil-dvorak-mode-map
   (kbd "TAB") #'indent-for-tab-command
@@ -94,14 +84,6 @@
                 "Switch to the previous buffer"
                 (interactive)
                 (switch-to-buffer nil))
-  "B" #'(lambda ()
-          "display all possible buffers and pick the one you want"
-          (interactive)
-          (list-buffers))
-  ;;(define-key evil-normal-state-map (kbd "Q") 'anzu-query-replace-regexp)
-  ;; This for when I use visual line mode mode. Now visual line mode should work for basic
-  ;; key movements
-  ;; This is not working.
   (kbd "h") #'evil-next-line
   (kbd "t") #'previous-line
   (kbd "n") #'backward-char
@@ -112,9 +94,6 @@
           (kill-line 0))
   "I" 'evil-append
   "$" 'ispell-word
-  (kbd "C-s") 'evil-substitute
-  "s" 'evil-forward-char
-  "n" 'evil-backward-char
   (kbd "C-l") 'recenter-top-bottom
   "l" 'recenter-top-bottom
   "o" 'evil-backward-word-begin
@@ -128,40 +107,15 @@
   (kbd "C-h") 'evil-open-below
   (kbd "C-t") 'evil-open-above
   (kbd "C-c r") 'evil-record-macro
-  ;; (kbd "C-w h") 'windmove-down
-  ;; (kbd "C-w t") 'windmove-up
-  ;; (kbd "C-w n") 'windmove-left
-  ;; (kbd "C-w s") 'windmove-right
-  "," 'undo-tree-undo
-  "/" 'helm-swoop
   "'" 'evil-goto-mark
   "Q" 'anzu-query-replace-regexp
-  (kbd "q") '(lambda ()
-               "q saves the current buffer, then kills it.  I should add a checking mechanism... If the buffer name starts and ends with *, then do not save the buffer"
-               (interactive)
-               (save-buffer)
-               (let (kill-buffer-query-functions) (kill-buffer)))
   (kbd "<backspace>") 'avy-goto-char
-  (kbd "l") 'recenter-top-bottom
-  ;;there is no need to set return to newline-and-indent, because electric-indent-mode is now on by default.
-  ;;at least so the documentation claimed
-  (kbd "<return>") 'newline-and-indent
-  (kbd "SPC") 'viper-space
-  (kbd "a") 'evil-first-non-blank
-  (kbd "A") 'evil-insert-line
-  (kbd "u") 'evil-end-of-line
-  (kbd "U") 'evil-append-line
-  (kbd "C-d") 'delete-char
-  (kbd "<") 'beginning-of-buffer
-  (kbd ">") 'end-of-buffer
-  (kbd "l") 'recenter-top-bottom
   ;;there is no need to set return to newline-and-indent, because electric-indent-mode is now on by default.
   (kbd "<return>") 'newline-and-indent
-  (kbd "SPC") 'viper-space
-  (kbd "C-a") 'mark-whole-buffer)
+  )
 
 
-(evil-define-key 'insert evil-dvorak-mode-map (kbd "C-i") 'info-display-manual
+(evil-define-key 'insert
   ;; this should prevent making the escape key moving the cursor backwards but...
   ;; (evil-define-key viper-insert-global-user-map
   ;;   (kbd "ESC") '(lambda()
@@ -176,53 +130,7 @@
   (kbd "C-t") 'evil-previous-line
   (kbd "C-n") 'backward-char
   (kbd "C-s") 'forward-char
-  (kbd "C-i") 'info-display-manual
   (kbd "C-c r") 'evil-record-macro)
-
-(defun evil-dvorak-turn-off-web-mode-key ()
-  "This turns off the evil-dvorak web-mod keyboard shortcuts."
-  (interactive)
-  (remove-hook 'web-mode-hook 'evil-dvorak-turn-on-web-mode-keys)
-  (define-key evil-normal-state-map "H" 'evil-window-top))
-
-(defun evil-dvorak-turn-on-web-mode-keys ()
-  "This turns on the evil-dvorak web-mode keyboard shortcuts."
-  (interactive)
-  (define-key evil-normal-state-map (kbd "Hta") 'web-mode-tag-attributes-sort)
-  (define-key evil-normal-state-map (kbd "Htb") 'web-mode-tag-beginning)
-  (define-key evil-normal-state-map (kbd "Hte") 'web-mode-tag-end)
-  (define-key evil-normal-state-map (kbd "Htm") 'web-mode-tag-match)
-  (define-key evil-normal-state-map (kbd "Htn") 'web-mode-tag-next)
-  (define-key evil-normal-state-map (kbd "Htp") 'web-mode-tag-previous)
-  (define-key evil-normal-state-map (kbd "Hts") 'web-mode-tag-select)
-  (define-key evil-normal-state-map (kbd "Hek") 'web-mode-element-kill)
-  (define-key evil-normal-state-map (kbd "Hev") 'web-mode-element-vanish)
-  (define-key evil-normal-state-map (kbd "Hea") 'web-mode-element-content-select)
-  (define-key evil-normal-state-map (kbd "Hec") 'web-mode-element-clone)
-  (define-key evil-normal-state-map (kbd "Heb") 'web-mode-element-beginning)
-  (define-key evil-normal-state-map (kbd "Hed") 'web-mode-element-child)
-  (define-key evil-normal-state-map (kbd "Hee") 'web-mode-element-end)
-  (define-key evil-normal-state-map (kbd "Hef") 'web-mode-element-children-fold-or-unfold)
-  (define-key evil-normal-state-map (kbd "Hei") 'web-mode-element-insert)
-  (define-key evil-normal-state-map (kbd "Hem") 'web-mode-element-mute-blanks)
-  (define-key evil-normal-state-map (kbd "Hen") 'web-mode-element-next)
-  (define-key evil-normal-state-map (kbd "Hep") 'web-mode-element-previous)
-  (define-key evil-normal-state-map (kbd "Her") 'web-mode-element-rename)
-  (define-key evil-normal-state-map (kbd "Hes") 'web-mode-element-select)
-  (define-key evil-normal-state-map (kbd "Het") 'web-mode-element-transpose)
-  (define-key evil-normal-state-map (kbd "Heu") 'web-mode-element-parent)
-  (define-key evil-normal-state-map (kbd "Hew") 'web-mode-element-wrap)
-  (define-key evil-normal-state-map (kbd "Hab") 'web-mode-attribute-beginning)
-  (define-key evil-normal-state-map (kbd "Hae") 'web-mode-attribute-end)
-  (define-key evil-normal-state-map (kbd "Hai") 'web-mode-attribute-insert)
-  (define-key evil-normal-state-map (kbd "Hak") 'web-mode-attribute-kill)
-  (define-key evil-normal-state-map (kbd "Han") 'web-mode-attribute-next)
-  (define-key evil-normal-state-map (kbd "Has") 'web-mode-attribute-select)
-  (define-key evil-normal-state-map (kbd "Hat") 'web-mode-attribute-transpose))
-
-(if (equal evil-dvorak-use-for-web-mode 1)
-    (add-hook 'web-mode-hook 'evil-dvorak-turn-on-web-mode-keys)
-  (remove-hook 'web-mode-hook 'evil-dvorak-turn-on-web-mode-keys))
 
 ;; I should not set the initial state for various modes, because some evil
 ;; users might want dired to be loaded in evil-normal-state.
